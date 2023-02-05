3ec77849c0116ca5da1e39d248d3bab90281f1d0
Bartlomiej Pedryc <b.pedryc@poczta.fm>
2023-02-05 12:57:10 +0100

    Add useMemo function for totalPrice

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,24 +3,22 @@ import styles from './Product.module.scss';
 import ProductImage from '../ProductImage/ProductImage'
 import ProductOptions from '../ProductOptions/ProductOptions';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const Product = props => {
 
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [extraPrice, setExtraPrice] = useState(0);
+    
+  const totalPrice = useMemo(() => extraPrice + props.basePrice, [extraPrice]);
 
-  let totalPrice;
-
-  const calculatePrice = () => totalPrice = props.basePrice + extraPrice;
-  
   return (
     <article className={styles.product}>
       <ProductImage title={props.title} name={props.name} currentColor={currentColor}/>
       <div>
         <header>
           <h2 className={styles.name}>{props.title}</h2>
-          <span className={styles.price}>{calculatePrice()}$</span>
+          <span className={styles.price}>{totalPrice}$</span>
         </header>
         <ProductOptions {...props} setExtraPrice={setExtraPrice} currentColor={currentColor} setCurrentColor={setCurrentColor} totalPrice={totalPrice}/>
       </div>
